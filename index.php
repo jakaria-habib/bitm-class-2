1bedc817c91c21cd4e7859ea16f3970e266d5728
jakaria-habib <jakaria.ruet.ete@gmail.com>
2024-02-16 01:20:15 +0600

    update 4th time

--- index.php
+++ index.php
@@ -1,6 +1,2 @@
 <?php
 echo "we are the creation of ALLAH.";
-echo "Allah is our creator and we will get jannah inshaAllah";
-echo "Allah is our creator and we will get jannah inshaAllah";
-echo "Allah is our creator and we will get jannah inshaAllah";
-echo "Allah is our creator and we will get jannah inshaAllah";
